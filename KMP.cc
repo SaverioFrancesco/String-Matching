098c7e91e1cbfdf7e4281c15da5f05325dabff9d
Francesco S. Comisso <mitramdhir@gmail.com>
2018-07-08 18:00:53 +0200

    KMP

---
+++ KMP.cc
@@ -0,0 +1,125 @@
+
+#include<iostream>
+#include <vector>
+#include <algorithm> // std::min_element
+#include <iterator>  // std::begin, std::end
+using namespace std;
+
+
+/*Versione Zantoni*/
+void KMP(string T,  string P){
+	
+
+	int m = T.length();
+	int n = P.length();
+
+	int SP[n];
+	int i, j =0;
+
+	for (int i = 0; i < n; ++i)
+	{
+
+		/*
+						   x              x?=y                    y
+		+----------------------------------------------------------------+
+		+-----------------+o                    +----------------+i
+				gamma      sp(i-1)=|gamma|              gamma
+
+		*/
+		
+		//j==i-1
+
+		//poi... j'=sp(i-1)-1
+
+
+		/*
+						   x                                      y
+		+----------------------------------------------------------------+
+		+-----------------+                     +----------------+i
+		                                                         j
+				gamma      sp(i-1)=|gamma|              gamma
+
+			 x'                        x'?=y                      y			  
+		+-----------------+                     +----------------+i
+		+---+o        +---+                     +---+o       +---+                                    
+			 j'
+		gamma'            
+		*/
+
+	/*  ESEMPIO:
+
+		abac.....abab
+
+		c!=a quindi cerco il amssimo prefisso/suffisso in aba nella speranza che sia seguito  da P[i] = b */
+		
+
+
+		j=i-1;
+		SP[i]=0;
+		while(P[SP[j]] != P[i] && j>0) j=SP[j]-1;
+
+		if(j>=0 && P[i]==P[SP[j]]) SP[i]=SP[j]+1;
+
+	}
+
+	#ifdef DEBUG
+	for (int i = 0; i < n; ++i)
+	{
+		cout <<P[i] <<  " -> " << SP[i] << endl;
+
+	}
+	#endif
+
+	int h=0;//posizione sul testo
+	int k=0;//posizione sul pattern			
+	while(h<=m-n)
+	{
+			while(T[h+k] == P[k] && k<n) k++;
+
+			if(k==n){ cout << "Pattern trovato in " << h<< endl; 
+			//ATTENZIONE ! SE DECIDO DI MUOVERE IL PATTERN IN AVANTI DI |P|=n==k posizioni RINUNCIO A TUTTE LE OCCORRENZE CHE INIZIANO COME SUFFISSO DI QUELLA CHE HO APPENA TROVATO.
+			/*h+=k; k=0;*/ } 
+			else{
+			//h+k è la pos del mismatch.
+			//sp[k-1] ti dice quanti caratteri dall'inizio del pattern non devi ricontrollare
+
+
+			/*h += SP[k-1]+1;
+			k = SP[k-1];
+			*/
+		}
+		#ifdef DEBUG
+		cout << "Miss match in posizione " << h+k << " e sul pattern "<< k << endl;
+		for (int i = 0; i < h+k; ++i)
+		{
+			cout << T[i];
+		}
+		cout << endl;
+		for (int i = 0; i < k; ++i)
+		{
+			cout << P[i];
+		}
+		cout << endl;
+		cout << "Shift di " << SP[k-1]+1<< " sul testo del pattern" << endl;
+		#endif
+
+		h += SP[k-1]+1;
+		k = SP[k-1];
+	}
+
+}
+
+// Driver program
+int main()
+{
+    string text = "michele michele michelle lelle rene irene imene michele ene michiremich"; 
+    string pattern = "irene";
+    KMP(text, pattern);
+
+    for (int i = 0; i < text.length(); ++i)
+    {
+    	cout << i << "--->"<< text[i]<<" "<<endl;
+    }
+    cout<<endl;
+    return 0;
+}
---
+++ zeta.c
@@ -0,0 +1,217 @@
+
+
+/*#ifndef DEBUG
+#define DEBUG value
+#endif*/
+
+#include<iostream>
+using namespace std;
+ 
+
+void zeta(string S, int Z[]);
+ 
+// prints all occurrences of pattern in text using Z algo
+void search(string text, string pattern)
+{
+    // Create concatenated string "P$T"
+    string concat = pattern + "$" + text;
+    int l = concat.length();
+
+    cout << concat << endl;
+ 
+    // Construct Z array
+    int Z[l];
+    zeta(concat, Z);
+ 
+    // now looping through Z array for matching condition
+    for (int i = 0; i < l; ++i)
+    {
+        // if Z[i] (matched region) is equal to pattern
+        // length we got the pattern
+        if (Z[i] >= pattern.length())
+            cout << "Pattern found at index "
+                << i << " of length " << Z[i] << endl;
+    }
+
+    for (int i = 0; i < l; ++i)
+    {
+    	/* code */
+
+    	cout << i << " - " << concat[i] << " - "  << Z[i] << " \n";
+    }
+
+}
+ 	
+
+void zeta(string S, int Z[]){
+
+	int n = S.length();
+
+	//Inizializzo Z
+	int i;
+	for ( i = 0; i <= n-1; i++) Z[i]=0;
+	//Lo Z blocco più a destra incontrato sin ora
+	int l=0;
+	int r=0;
+
+	for (i=1; i <= n-1; i++){
+
+		#ifdef DEBUG
+		cout << endl << "posizione i = " << i  << endl;
+		cout << " z blocco = [" << l << ", " << r << "]" << endl; 
+		#endif
+		//int eccedendo=0;
+
+		if(i<=r){//Caso in cui i e' dentro nell'ultimo z blocco
+
+
+			#ifdef DEBUG
+			cout<< " i e' dentro lo z blocco ..." << endl;
+			cout << " Lo z blocco e' lungo r ="<< r << " meno l = " << l <<" +1 cioe' : " << r-l+1 << endl;
+			#endif
+			
+			//Qui discrimino i 3 casi (beta sottostringa dello z blocco alpha, beta suffisso dello z blocco e  beta che sfora lo zeta blocco)->
+
+			int k = i-l;
+			
+			#ifdef DEBUG
+			cout << "i cade nel prefisso del testo in posizione k = " << k << endl;
+			#endif
+			
+			if (k+Z[k]/*-1*/  /*in Z[k] conto anche il carattere T[k] quindi -1*/<r-l+1 /*l e r sono entrambi dentro lo z blocco quindi sottraendoli mangio un carattere quindi +1*/
+						) 
+			// La lunghezza di aplha e' r-l+1 
+			{
+
+				if(i+Z[k]< S.length()){
+				
+					#ifdef DEBUG
+					cout << "Copio il valore di Z dalla posizione k = "<< k << " alla posizione i = " << i << endl;
+					#endif
+					
+					Z[i]=Z[k];
+				
+				}else{
+					
+					#ifdef DEBUG
+					cout << "Starei eccedendo..."<< endl;
+					cout << "Copio il valore di Z dalla posizione k = "<< k << " alla posizione i = " << i << endl;
+					#endif
+					
+					Z[i]= S.length()-i;
+				}
+
+				#ifdef DEBUG
+				cout << "Questo era il caso contenuto strettamente , valore finale di Z[i]="<< Z[i] << endl;
+				#endif
+			}
+			if (k+Z[k]/*-1*/ ==r-l+1)
+				{
+
+					Z[i]= r-i+1;
+
+					#ifdef DEBUG
+					cout << "Meglio procedere con i confronti...";
+					cout  << "Li ricomincio fra r+1 = "<< r+1<< "e  r-l+1 =" <<  r-l+1 << endl;
+					#endif
+
+					int start1 = r+1; //r+1;
+					int start2 = r-i+1;  //la lunghezza di gamma ;
+					int jj=0;
+					
+					while(S[start1+jj] == S[start2+jj] && start1+jj < S.length()) {
+						Z[i]++;
+						jj++;
+					}
+
+					if(jj>0){
+
+						l=i;
+						r=i+Z[i]-1;
+
+						#ifdef DEBUG
+						cout << "Aggiorno lo Z blocco"<< endl;
+						#endif
+
+					}
+
+					#ifdef DEBUG
+					cout << "Questo era il caso gamma suffisso alpha, valore finale di Z[i]="<< Z[i] << endl;
+					#endif
+
+			}
+
+			if (k+Z[k]>r-l+1 ){ 
+
+				#ifdef DEBUG
+				cout << "k + Z[k] eccede lo z blocco ... "<< k + Z[k] <<" > "<< r-l+1 << endl;
+				#endif
+
+				Z[i]=r-i+1; // la lunghezza di gamma. Ricorda, considero sempre interni gli indici che delimitano una stringa.
+
+				/*Si noti che ora k+Z[i] vale esattamente la lunghezza di alpha cioe' i -l +  r -i + 1 */
+				
+				int start1 = r+1;
+				int start2 = r-l +1;
+				int jj=0;
+
+				#ifdef DEBUG
+				cout << "Sarebbe meglio procedere con i confronti...";
+				cout  << "Li ricomincio fra k+1 = "<< k+Z[k]+1<< "e i+Z[i]+1 =" << i+Z[i]+1 << endl;
+				#endif
+				
+				while(S[start1+jj] == S[start2+jj]) {
+					Z[i]++;
+					jj++;
+				}
+
+				if(jj>0){
+
+					l=i;
+					r=i+Z[i]-1;
+				
+					#ifdef DEBUG
+					cout << "Aggiorno lo Z blocco"<< endl;
+					#endif
+				
+				}
+
+			}
+
+		}else{// caso in cui i e' oltre lo z blocco (continuo i confronti regolarmente)
+
+			#ifdef DEBUG
+			cout << "i e' fuori lo z blocco ..." << endl;
+			#endif
+
+			int j=0;
+
+			while(S[i+j]==S[j]){
+
+				Z[i]++;
+				j++;
+			}
+
+			if(j>0){
+				//se siamo andati almeno un passo avanti:
+				
+				#ifdef DEBUG
+				cout << "siamo andati avanti con i confronti per j =" << j << " posizioni." << endl;				
+				#endif
+				
+				r=i+Z[i]-1;
+				l=i;
+			}
+
+		}
+	}
+
+}
+
+// Driver program
+int main()
+{
+    string text = "ababababababababababababa" ;   string pattern = "aba";
+    search(text, pattern);
+    return 0;
+}
